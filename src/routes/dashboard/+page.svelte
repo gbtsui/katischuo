d38f167a1fc85217e3d2111039170245ba676101
gbtsui <gabriel.tsui@protonmail.com>
2026-03-18 16:20:01 -0400

    feat: dashboard skeleton. i actually really like the vibes and colors, it definitely needs a lot more polish though - need to reconfigure a bit so i can fit my desired aesthetic and layout, but i went through a few different variations before getting here

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let currentTime: Date = $state(new Date);
+
+	let currentHour = $derived(currentTime.getHours());
+	let currentMinute = $derived(currentTime.getMinutes());
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			currentTime = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<div class="w-[100vw] h-[100vh] flex bg-stone-900 text-stone-50">
+	<div class="w-[40vw] left-0 h-[100vh] flex">
+		<div class="m-[5vw] flex flex-col">
+			<div class="w-[30vw] bg-stone-700">
+				Dashboard
+			</div>
+			<!--put ischyros here!!-->
+			<!--or, i guess, Karolos Petros Axios, "Ischyros the Whitefist"-->
+		</div>
+
+		<!--also need to insert streaks, etc-->
+	</div>
+
+	<div class="w-[60vw] right-0 h-[100vh] flex">
+		<!--okay i had a layout in mind and then i realized it was kinda cooked so im gonna remake it-->
+		<div class="w-[55vw] h-[90vh] ml-[5vw] my-[5vh] flex flex-col gap-[5vh]">
+			<!--track workout card thingy-->
+			<div
+				class="right-0 h-[20vh] bg-stone-800 border border-stone-700 flex text-center align-center items-center justify-center cursor-pointer flex flex-row gap-[2.5vw]">
+				<div class="text-stone-500 text-4xl">
+					{currentHour}:{currentMinute}
+				</div>
+				<div class="text-5xl">
+					track workout
+				</div>
+			</div>
+
+			<!--track weight-->
+			<div
+				class="right-0 h-[20vh] bg-stone-800 border border-stone-700 flex text-center align-center items-center justify-center cursor-pointer flex flex-row gap-[2.5vw]">
+				<div class="text-5xl">
+					track weight
+				</div>
+			</div>
+
+			<!--settings-->
+			<div
+				class="right-0 h-[20vh] bg-stone-800 border border-stone-700 flex text-center align-center items-center justify-center cursor-pointer flex flex-row gap-[2.5vw]">
+				<div class="text-5xl">
+					settings
+					<!--i might actually want to make this one smaller and just a cog?-->
+				</div>
+			</div>
+			<!--stats-->
+			<!--make this later but it should be like a big thing and display-->
+
+			<!--stats at a glance?-->
+
+
+		</div>
+	</div>
+</div>
+
+<!--the world needs... only one big boss-->
