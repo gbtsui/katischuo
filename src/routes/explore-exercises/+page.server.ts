c9fa620647abb06efeae8ce5a588ea9246fa101b
gbtsui <gabriel.tsui@protonmail.com>
2026-03-19 17:15:08 -0400

    feat: YES FINALLY WORKING EXERCISE CREATOR??? had a headache trying to access session on backend

--- src/routes/explore-exercises/+page.server.ts
+++ src/routes/explore-exercises/+page.server.ts
@@ -1,12 +1,52 @@
-import type { PageServerLoad } from '../../../.svelte-kit/types/src/routes/track/$types';
-import { exercise, exerciseCategoryEnum, muscleGroupEnum, strengthExerciseEquipmentEnum } from '$lib/server/db/schema';
+import type { PageServerLoad } from './$types';
+import {
+	exercise,
+	exerciseCategoryEnum,
+	type InsertExercise,
+	muscleGroupEnum,
+	strengthExerciseEquipmentEnum
+} from '$lib/server/db/schema';
+import type { Actions } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
+import { fail } from '@sveltejs/kit';
+import { randomUUID } from 'crypto';
 
-export const load: PageServerLoad = async ({params}) => {
-
+export const load: PageServerLoad = async () => {
 	return {
 		exerciseCategories: exerciseCategoryEnum.enumValues,
 		muscleGroups: muscleGroupEnum.enumValues,
 		equipment: strengthExerciseEquipmentEnum.enumValues,
-	}
-}
+	};
+};
 
+export const actions: Actions = {
+	create: async ({ request, locals }) => {
+		const data = await request.formData();
+		const session = locals.session
+
+		if (!session || !session.user) return fail(401, {message: "not signed in!!"})
+
+		const exerciseName = data.get('exercise_name');
+		const category = data.get('category');
+		const primaryMuscle = data.getAll('primary_muscle');
+		const secondaryMuscles = data.getAll('secondary_muscles');
+		const equipment = data.get('equipment');
+
+		if (!exerciseName || !category || !primaryMuscle.length) {
+			return fail(400, { message: "missing required fields" });
+		}
+
+		const newExercise: InsertExercise = {
+			id: randomUUID(),
+			exerciseName: exerciseName as string,
+			category: category as typeof exerciseCategoryEnum.enumValues[number],
+			primaryMuscle: primaryMuscle as  typeof muscleGroupEnum.enumValues[number][],
+			secondaryMuscles: secondaryMuscles as typeof muscleGroupEnum.enumValues[number][],
+			equipment: equipment ? equipment as typeof strengthExerciseEquipmentEnum.enumValues[number] : null,
+			isCustom: true,
+			createdByUserId: session.user.id
+		};
+
+		await db.insert(exercise).values(newExercise);
+	}
+};
--- src/routes/explore-exercises/+page.svelte
+++ src/routes/explore-exercises/+page.svelte
@@ -6,23 +6,34 @@
 <div>
 	<div>i am too lazy to style things right now but i will do it eventually astaghfirullah</div>
 
-	<form>
+	<form method="POST" action="?/create">
 		<div><input type="text" name="exercise_name" id="exercise_name" placeholder="exercise name" required /></div>
 		<div><select name="category" id="category" required>
 			{#each exerciseCategories as exerciseCategory (exerciseCategory)}
 				<option value={exerciseCategory}>{exerciseCategory}</option>
 			{/each}
 		</select></div>
+
 		<div>
+			<p>Primary Muscle</p>
 			{#each muscleGroups as muscleGroup (muscleGroup)}
-				<input type="checkbox" value={muscleGroup}>{muscleGroup}/>
+				<label>
+					<input type="checkbox" name="primary_muscle" value={muscleGroup} />
+					{muscleGroup}
+				</label>
 			{/each}
 		</div>
+
 		<div>
+			<p>Secondary Muscles</p>
 			{#each muscleGroups as muscleGroup (muscleGroup)}
-				<input type="checkbox" value={muscleGroup}>{muscleGroup}/>
+				<label>
+					<input type="checkbox" name="secondary_muscles" value={muscleGroup} />
+					{muscleGroup}
+				</label>
 			{/each}
 		</div>
+
 		<div>
 			<select name="equipment" id="equipment">
 				{#each equipment as equipmentName (equipmentName)}
@@ -30,5 +41,7 @@
 					{/each}
 			</select>
 		</div>
+
+		<button type="submit">create exercise</button>
 	</form>
 </div>
