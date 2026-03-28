1d75493922e9654bd702228065c1f04e077ccabf
gbtsui <gabriel.tsui@protonmail.com>
2026-03-28 14:38:59 -0400

    feat: weight recording now works

--- src/routes/weight-manager/+page.svelte
+++ src/routes/weight-manager/+page.svelte
@@ -7,8 +7,11 @@
 
 	let weightRecords: SelectTrackedWeightDataPoint[] = $state([]);
 
-	let weightInput: number | null = $state(null)
-	let notesInput: string = $state("")
+	let weightInput: number | null = $state(null);
+	let notesInput: string = $state('');
+	let weightUnitInput: "lbs" | "kg" = $state("lbs") //TODO: add function to query for actual preferred weight unit
+
+	let trackLoading = $state(false);
 
 	const updateWeightRecords = async () => {
 		//const res = await fetch(resolve("/api/"))
@@ -16,6 +19,29 @@
 		weightRecords = res.records;
 	};
 
+
+	const handleRecordWeight = async () => {
+		if (!weightInput) return;
+
+		trackLoading = true;
+		const res = await fetch(resolve('/api/create-weight-record'), {
+			method: 'POST',
+			body: JSON.stringify({
+				weight: weightInput,
+				notes: notesInput,
+				weightUnit: weightUnitInput,
+			})
+		});
+		if (!res.ok) {
+			console.error(res)
+		}
+
+		await updateWeightRecords()
+		weightInput = null;
+		notesInput = "";
+
+		trackLoading = false
+	};
 
 
 	onMount(async () => {
@@ -36,7 +62,7 @@
 		<!--history section-->
 		<div class="flex flex-col gap-[1rem]">
 			<div class="text-xl">Historical Data</div>
-			<div class="w-[37.5vw] h-[30vh] bg-stone-800 overflow-y-scroll 			[&::-webkit-scrollbar]:w-[1vw]
+			<div class="w-[37.5vw] h-[30vh] bg-stone-800 overflow-y-scroll [&::-webkit-scrollbar]:w-[1vw]
   [&::-webkit-scrollbar-track]:bg-stone-500
   [&::-webkit-scrollbar-thumb]:bg-stone-800
 		[&::-webkit-scrollbar]:mx-[1vw]" transition:fly={{delay: 67*2}}>
@@ -64,23 +90,31 @@
 [&::-webkit-scrollbar]:mx-[1vw]" transition:fly={{delay: 67}}>
 				<div class="p-[2rem] flex flex-col gap-[2rem] justify-between ">
 					<div class="flex flex-col gap-[2rem]">
-						<div >
+						<div>
 							<div>Current Weight</div>
-							<input bind:value={weightInput} type="number" placeholder="100.0" class="bg-stone-700 focus:border-none focus:outline-none"/>
-							<select class="bg-stone-700">
-								<option>lbs</option>
-								<option>kg</option>
+							<input bind:value={weightInput} type="number" placeholder="100.0"
+										 class="bg-stone-700 focus:border-none focus:outline-none" />
+							<select class="bg-stone-700" bind:value={weightUnitInput}>
+								<option value="lbs">lbs</option>
+								<option value="kg">kg</option>
 							</select>
 						</div>
 						<div>
 							<div>Notes</div>
-							<textarea bind:value={notesInput} class="bg-stone-700 resize-none field-sizing-content min-h-[5rem] max-h-[30rem] w-full"></textarea>
+							<textarea bind:value={notesInput}
+												class="bg-stone-700 resize-none field-sizing-content min-h-[5rem] max-h-[30rem] w-full"></textarea>
 						</div>
 					</div>
-
-					<div>
-						<button>Track!</button>
-					</div>
+					{#if !trackLoading}
+						<div>
+							<button
+								class="p-[1rem]  hover:text-2xl transition-all text-xl {weightInput === null || weightInput === 0 ?  'bg-stone-900 cursor-not-allowed text-stone-400' : 'bg-stone-700 hover:p-[1.5rem] cursor-pointer' }"
+								onclick={handleRecordWeight}>Track!
+							</button>
+						</div>
+					{:else }
+						<div>Loading...</div>
+					{/if}
 				</div>
 			</div>
 		</div>
